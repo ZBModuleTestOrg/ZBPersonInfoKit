6d42145c805a46aeeae70661a29fd5afd53e5a4e
admin <>
2020-07-07 16:51:49 +0800

    ZBPersonInfoKit 组件提交 01

---
+++ Example/Pods/Target Support Files/Pods-ZBPersonInfoKit_Tests/Pods-ZBPersonInfoKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZBPersonInfoKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZBPersonInfoKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZBPersonInfoKit/ZBPersonInfoKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZBPersonInfoKit : NSObject
+@end
+@implementation PodsDummy_ZBPersonInfoKit
+@end
